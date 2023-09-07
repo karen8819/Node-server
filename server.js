573ec5120087b70cc26945b76e0c5e3e53063d69
Karen Paguada <135404251+karen8819@users.noreply.github.com>
2023-09-07 07:31:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,23 +16,7 @@ servidor.listen(puerto, host, () => {
 
 
 
-// const chalk ='chalk';
-// const http = require('http');
 
-
-// const host = 'localhost';
-// const puerto = 8000;
-
-// const servidor = http.createServer((_req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   const respuesta = JSON.stringify('Hola desde mi servidor' );
-//   res.end(respuesta);
-// });
-
-
-// servidor.listen(puerto, host, () => {
-//   console.log(chalk.green(`El servidor está escuchando en http://${host}:${puerto}`));
-// });
 
 
 
